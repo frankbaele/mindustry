5fef6bfc729e1d0849aebaeb0de386bcc66b39fa
frank <frankbaele@gmail.com>
2019-10-17 20:43:31 +0200

    hardcode mindustry url

--- latest_release.sh
+++ latest_release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+  curl --silent "https://api.github.com/repos/Anuken/Mindustry/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
-get_latest_release "Anuken/Mindustry"
+
+get_latest_release
