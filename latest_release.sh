df7fee41b2fd6846fd3da41de14fa39617f908da
frank <frankbaele@gmail.com>
2019-10-17 20:33:41 +0200

    add script

---
+++ latest_release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+get_latest_release "Anuken/Mindustry"
