2f975c2798d1eab5955297755a2ed73b86c5c755
Frank Baele <frank.baele@digipolis.be>
2019-10-18 08:41:08 +0200

    add git commands

--- script/index.js
+++ script/index.js
@@ -1,23 +1,31 @@
 const Octokit = require("@octokit/rest");
 const octokit = new Octokit({
-  auth: process.env.ACCESS_TOKEN
+  username: "frankbaele",
+  password: process.env.PASSWORD
 });
+
+const simpleGit = require('simple-git')('../');
 const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-async function updateDockerFile(version){
+async function updateDockerFile(version) {
   const file = await readFile('./Dockerfile.template', 'utf8');
   const result = file.replace(/%%release%%/g, version);
   await writeFile('../Dockerfile', result)
 }
 
-async function createCommit(){
-
+async function createCommit(version) {
+  return simpleGit
+  .add('./Dockerfile')
+  .commit("Update to new version: " + version)
+  .tag(version)
+  .push()
+  .pushTags()
 }
 
-async function setTag(version){
+async function setTag(version) {
   // octokit.git.createTag({
   //   "frankbaele",
   //   "mindustry",
@@ -27,26 +35,28 @@ async function setTag(version){
   //   "commit"
   // })
 }
-async function creatRelease (version) {
+
+async function creatRelease(version) {
 
 }
 
 (async () => {
   const mindustry_release = await octokit.repos.getLatestRelease({
-    owner:"Anuken",
-    repo:"Mindustry"
-  })
+    owner: "Anuken",
+    repo: "Mindustry"
+  });
 
   const docker_release = await octokit.repos.getLatestRelease({
-    owner:"frankbaele",
-    repo:"mindustry"
-  })
+    owner: "frankbaele",
+    repo: "mindustry"
+  });
 
-  if(mindustry_release.data.tag_name !== mindustry_release.data.docker_release){
+  if (mindustry_release.data.tag_name !== mindustry_release.data.docker_release) {
 
   }
-  await updateDockerFile(mindustry_release.data.tag_name);
-  await setTag(mindustry_release.data.tag_name);
-  await creatRelease(mindustry_release.data.tag_name);
-
+  const newVersion = "v100"
+  await updateDockerFile(newVersion);
+  await setTag(newVersion);
+  await creatRelease(newVersion);
+  await createCommit(newVersion)
 })();
